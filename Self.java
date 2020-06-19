ffbb2c2685615426c5cdbe253bddbeeb2da7c270
SelfRidicule <selfridicule@gmail.com>
2020-06-19 22:38:57 +0800

    bad

--- Self.java
+++ Self.java
@@ -3,6 +3,7 @@ public class Self{
     public static void main(String args[]){
         System.out.println("hello");
         System.out.println("word");
+        System.out.println("bad");
     }
 
 }

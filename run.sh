6aa229afaedc708c80513f925bdf8581f480e18f
SelfRidicule <selfridicule@gmail.com>
2020-06-21 14:05:09 +0800

    自动化打包maven-->解压-->运行解压版本maven 构建项目

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-javac Self.java
 
-OUTPUT=$(java Self)
-if [[ "$OUTPUT" == *"bad"* ]]; then
-  echo "bad!"
-  exit 1
-fi
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
-echo "good!"
-exit 0
+rm -rf apache-maven-* &&\
+
+mvn clean package  -DskipTests -Drat.skip=true &&\
+
+unzip apache-maven/target/apache-maven-$VERSION-bin.zip  -d . &&\
+
+/Users/wangqichao/git/maven/apache-maven-$VERSION/bin/mvn  -f /Users/wangqichao/sourcetree/selfweb/pom.xml compile
